ff07da52dc0f7bc35978bda81c72b71408d86a60
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-14 00:02:41 +0700

    clean up misc file

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@config": "./app.json",
+            "@assets": "./assets",
+            "@models": "./models",
+            "@screens": "./screens",
+            "@helpers": "./helpers",
+            "@contexts": "./contexts",
+            "@services": "./services",
+            "@constants": "./constants",
+            "@components": "./components",
+            "@navigations": "./navigations",
+          },
+        },
+      ],
+    ],
   };
 };
