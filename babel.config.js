97087f54f4c1305ed61bdb47823f92c4339c2828
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-22 14:40:46 +0700

    chore(misc): update new system

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            "@config": "./app.json",
-            "@assets": "./assets",
-            "@models": "./models",
-            "@screens": "./screens",
-            "@helpers": "./helpers",
-            "@contexts": "./contexts",
-            "@services": "./services",
-            "@constants": "./constants",
-            "@components": "./components",
-            "@navigations": "./navigations",
-          },
+      ["module-resolver", {
+        root: ["./"],
+        alias: {
+          "@assets": "./src/assets",
+          "@models": "./src/models",
+          "@helpers": "./src/helpers",
+          "@screens": "./src/screens",
+          "@contexts": "./src/contexts",
+          "@services": "./src/services",
+          "@constants": "./src/constants",
+          "@images": "./src/assets/images",
+          "@components": "./src/components",
+          "@navigations": "./src/navigations",
         },
-      ],
+      }],
     ],
   };
 };
