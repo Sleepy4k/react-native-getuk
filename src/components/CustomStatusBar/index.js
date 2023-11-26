7c23f63c513dc77100f830d4ca33688b7d38394f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-26 10:59:14 +0700

    chore(component): change status bar background color

--- src/components/CustomStatusBar/index.js
+++ src/components/CustomStatusBar/index.js
@@ -36,7 +36,7 @@ CustomStatusBar.defaultProps = {
   animated: true,
   translucent: false,
   barStyle: "dark-content",
-  backgroundColor: colors.lightDeepBlue,
+  backgroundColor: colors.white,
   networkActivityIndicatorVisible: false,
 };
 
