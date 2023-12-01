b93faaf608f6031fd0994b123745590ce8f4683b
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-01 20:58:15 +0700

    chore(context): update auth context

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,27 +1,28 @@
-import * as React from "react";
+import { notification } from "@helpers";
+import { useState, useEffect, createContext } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const AuthContext = React.createContext();
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [logged, setLogged] = React.useState(false);
-  const [isAdmin, setIsAdmin] = React.useState(false);
-  const [location, setLocation] = React.useState(null);
-  const [userData, setUserData] = React.useState(null);
+  const [logged, setLogged] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [userData, setUserData] = useState(null);
 
   const isLoggedIn = async () => {
     try {
       let user = await AsyncStorage.getItem("authUser");
 
       if (user) {
-        setLogged(true);
         user = JSON.parse(user);
 
         if (user.role === "admin") setIsAdmin(true);
 
+        setLogged(true);
         setUserData(user);
       }
     } catch (error) {
+      notification.show("Error while check user data", "Error");
       console.log(`error while checking login status: ${error}`);
     }
   };
@@ -29,29 +30,33 @@ export const AuthProvider = ({ children }) => {
   const setLoggedIn = async (data) => {
     try {
       await AsyncStorage.setItem("authUser", JSON.stringify(data));
-      setLogged(true);
 
       if (data.role === "admin") setIsAdmin(true);
 
+      setLogged(true);
       setUserData(data);
     } catch (error) {
+      notification.show("Error while set user data", "Error");
       console.log(`error while login: ${error}`);
     }
   };
 
   const setLoggedOut = async (navigation) => {
     try {
-      await AsyncStorage.removeItem("authUser");
       setLogged(false);
       setIsAdmin(false);
       setUserData(null);
-      navigation.replace("Landing");
+
+      await AsyncStorage.removeItem("authUser");
     } catch (error) {
+      notification.show("Error while clear user data", "Error");
       console.log(`error while logout: ${error}`);
+    } finally {
+      navigation.replace("Landing");
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     isLoggedIn();
   }, []);
 
@@ -61,8 +66,6 @@ export const AuthProvider = ({ children }) => {
         logged,
         isAdmin,
         userData,
-        location,
-        setLocation,
         setLoggedIn,
         setLoggedOut
       }}
