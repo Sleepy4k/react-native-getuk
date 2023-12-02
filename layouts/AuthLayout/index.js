0cad06ceb3364ab8465bc45c9a57174b1dd7b0fd
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-02 15:53:42 +0700

    chore(layout): clean up auth layout

--- layouts/AuthLayout/index.js
+++ layouts/AuthLayout/index.js
@@ -1,10 +1,34 @@
-import { View, SafeAreaView } from 'react-native';
+import { Loader } from '@components';
+import { AuthContext } from '@contexts/AuthContext';
+import { useState, useEffect, useContext } from 'react';
+import { View, Animated, SafeAreaView } from 'react-native';
 
-export default function AuthLayout({ style, children }) {
-  return(
+export default function AuthLayout({ containerStyle, animatedStyle, header, children }) {
+  const { loading } = useContext(AuthContext);
+  const [slideAnim] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(slideAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  }, [slideAnim]);
+
+  const translateY = slideAnim.interpolate({
+    inputRange: [0, 3],
+    outputRange: [85, 0],
+  });
+
+  return (
     <SafeAreaView>
-      <View style={style}>
-        {children}
+      <View style={containerStyle}>
+        {loading && <Loader />}
+        {header}
+
+        <Animated.View style={[animatedStyle, { transform: [{ translateY }] }]}>
+          {children}
+        </Animated.View>
       </View>
     </SafeAreaView>
   )
