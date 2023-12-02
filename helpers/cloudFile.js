705ae301893bfb1f1765cd531e43c099b4dfad46
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-12-02 15:53:00 +0700

    chore(helper): clean up cloud file handler

--- helpers/cloudFile.js
+++ helpers/cloudFile.js
@@ -1,17 +1,17 @@
 import storage from "@services/storage";
 import { ref, uploadBytes, getDownloadURL, deleteObject } from "@firebase/storage";
 
-export const uploadFile = (file) => {
+export const uploadFile = async (file) => {
   const imgName = "img-" + new Date().getTime() + ".png";
   const storageRef = ref(storage, imgName);
   const metadata = {
     contentType: "image/png"
   };
 
-  const uploadTask = uploadBytes(storageRef, file, metadata);
-
-  uploadTask.then((snapshot) => {
-    console.log(`Uploaded a blob or file! ${snapshot}`);
+  await uploadBytes(storageRef, file, metadata).then((snapshot) => {
+    console.log(`Uploaded a blob or file! ${JSON.stringify(snapshot.metadata)}`);
+  }).catch((error) => {
+    console.log(`error while upload file: ${error}`);
   });
 
   return imgName;
@@ -30,7 +30,7 @@ export const getFile = async (fileName) => {
 export const deleteFile = async (fileName) => {
   const storageRef = ref(storage, fileName);
 
-  deleteObject(storageRef).then(() => {
+  await deleteObject(storageRef).then(() => {
     console.log(`File ${fileName} deleted`);
   }).catch((error) => {
     console.log(`error while delete file: ${error}`);
